f297a4846b884b275873b7da4efcd3b18521e898
Marion <marion@marion.ee>
2021-05-06 18:00:58 +0300

    Initial commit

---
+++ src/main/java/com/example/datanorExample/DatanorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.datanorExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatanorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatanorExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datanorExample/DatanorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datanorExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatanorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
